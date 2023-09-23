d2c3750e7ab313809f1c6371bb757d665ace5d00
dianabreik <diana.sam341@gmail.com>
2023-09-23 11:33:49 +0200

    add integration test for addToDo()

--- backend/src/test/java/com/example/backend/ToDoControllerTest.java
+++ backend/src/test/java/com/example/backend/ToDoControllerTest.java
@@ -4,13 +4,13 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -45,7 +45,31 @@ class ToDoControllerTest {
 """));
 
     }
+   @Test
+   @DirtiesContext
+    public void addToDo() throws Exception {
+        //GIVEN
+       //WHEN
+       mockMvc.perform(MockMvcRequestBuilders.post("/api/todo")
+               .contentType(MediaType.APPLICATION_JSON)
+               .content(""" 
+                           {
+                           "description": "testAddToDo",
+                           "status": "OPEN"
+                           }
+"""))
+       //THEN
+               .andExpect(status().isOk())
+               .andExpect(content().json("""
 
+{
+//ich muss da die ID nicht geben , weil sie da generiert wird und ich kenn sie gar nicht , aber was wichtig für mich ist, dass die ID schon existiert wird, das stelle ich mir sicher, anhand der nächsten Zeile mit andExpect.
+                           "description": "testAddToDo",
+                           "status": "OPEN"
+                           }
+"""))
+               .andExpect(jsonPath("$.id").isNotEmpty());
+   }
 
 
 }
